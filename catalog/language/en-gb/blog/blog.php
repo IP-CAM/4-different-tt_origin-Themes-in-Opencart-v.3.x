5cd275487e52400c17213a53b2634c962ea5b9ba
Sarah86 <sarah.goncalves@gmail.com>
2020-01-23 22:17:57 +0100

    copia do tema para edicao e ocslideshow.wig

---
+++ catalog/language/en-gb/blog/blog.php
@@ -0,0 +1,9 @@
+<?php
+// Text
+$_['button_read_more']         		 = 'Continue Reading';
+$_['text_empty']           			 = 'No articles';
+$_['text_headingtitle']           			 = 'Latest From Our blog';
+$_['title_blog']           			 = 'From Our Blog';
+$_['text_blog'] = 'Blog';
+$_['text_blog'] = 'Blog';
+$_['view_all'] = '<a href="index.php?route=blog/blog">View all <i class="icon-chevrons-right"></i></a>';
---
+++ catalog/language/en-gb/extension/module/ocfeaturedcategory.php
@@ -0,0 +1,4 @@
+<?php
+// Heading
+$_['heading_title'] = 'Top Categories ';
+$_['text_products'] = 'Products';
